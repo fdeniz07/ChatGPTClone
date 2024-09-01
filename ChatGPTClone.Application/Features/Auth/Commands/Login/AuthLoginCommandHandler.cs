fc9e099ddd9472c6b14a567866cb79ce2c46391a
Fatih Deniz <deniz.fatih@hotmail.de>
2024-09-01 20:52:10 +0200

    Added  Identity and JWT infrastructure

---
+++ ChatGPTClone.Application/Features/Auth/Commands/Login/AuthLoginCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.Application.Common.Models.General;
+using ChatGPTClone.Application.Common.Models.Identity;
+using MediatR;
+
+namespace ChatGPTClone.Application.Features.Auth.Commands.Login
+{
+    public class AuthLoginCommandHandler : IRequestHandler<AuthLoginCommand, ResponseDto<AuthLoginDto>>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthLoginCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<ResponseDto<AuthLoginDto>> Handle(AuthLoginCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _identityService.LoginAsync(new IdentityLoginRequest(request.Email, request.Password), cancellationToken);
+
+            return new ResponseDto<AuthLoginDto>(AuthLoginDto.FromIdentityLoginResponse(response));
+        }
+    }
+}
---
+++ ChatGPTClone.Application/Features/Auth/Commands/Login/AuthLoginCommandValidator.cs
@@ -0,0 +1,43 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.Application.Common.Models.Identity;
+using FluentValidation;
+
+namespace ChatGPTClone.Application.Features.Auth.Commands.Login
+{
+    public class AuthLoginCommandValidator : AbstractValidator<AuthLoginCommand>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthLoginCommandValidator(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public AuthLoginCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required")
+                .Length(5, 100)
+                .WithMessage("Email must be between 5 and 100 characters");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password is required")
+                .Length(6, 50)
+                .WithMessage("Password must be between 6 and 50 characters");
+
+            RuleFor(x => x)
+                .MustAsync(BeValidUser)
+                .WithMessage("Invalid email or password");
+        }
+
+        private Task<bool> BeValidUser(AuthLoginCommand model, CancellationToken cancellationToken)
+        {
+            var request = new IdentityAuthenticateRequest(model.Email, model.Password);
+
+            return _identityService.AuthenticateAsync(request, cancellationToken);
+        }
+
+    }
+}
