f72d7e73e4e412e1b102aec74b418a5f15dc24ad
Fatih Deniz <deniz.fatih@hotmail.de>
2024-09-28 23:11:04 +0200

    Cascading Authentication State in Blazor .NET 8

--- ChatGPTClone.WasmClient/Program.cs
+++ ChatGPTClone.WasmClient/Program.cs
@@ -1,6 +1,9 @@
+using Blazored.LocalStorage;
 using ChatGPTClone.WasmClient;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +12,18 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var baseApiUrl = builder.Configuration.GetValue<string>("BaseApiUrl")!;
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseApiUrl) });
+
+builder.Services.AddBlazoredLocalStorage(config =>
+{
+    config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+    config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+    config.JsonSerializerOptions.WriteIndented = false;
+});
+
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
